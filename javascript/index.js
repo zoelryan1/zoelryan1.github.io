6b822400b4b92758d63353ae0bcaf9fc62e5adec
Zoe <zoeryan@comcast.net>
2018-10-12 19:22:11 -0700

    new listener

--- javascript/index.js
+++ javascript/index.js
@@ -1,16 +1,15 @@
-// use an eventlistener for the event
-var subButton = document.getElementById('subButton');
-subButton.addEventListener('click', getInput, false);
-
 // the function which handles the input field logic
 function getInput() {
     var nameField = document.getElementById('textField).value;
     var result = document.getElementById('result');
-
-function WriteToFile(passForm) {
     set fso = CreateObject("Scripting.FileSystemObject"); 
     set s = fso.CreateTextFile("/home/zoe/Desktop/test.txt", True);
     s.writeline(result);
     s.Close();
- }
+}
+
+
+// use an eventlistener for the event
+    var subButton = document.getElementById('subButton');
+    subButton.addEventListener('click', getInput, false);
 
