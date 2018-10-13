e06a142e741d88073261598829a3f02c2379e5fd
Zoe <zoeryan@comcast.net>
2018-10-12 18:24:36 -0700

    make text

--- javascript/index.js
+++ javascript/index.js
@@ -1,11 +1,8 @@
 // the function which handles the input field logic
-function getUserName() {
-    var nameField = document.getElementById('nameField').value;
+function getInput() {
+    var nameField = document.getElementById('textField).value;
     var result = document.getElementById('result');
-    
-    if (nameField.length < 3) {
-        result.textContent = 'Username must contain at least 3 characters';
 
 // use an eventlistener for the event
 var subButton = document.getElementById('subButton');
-subButton.addEventListener('click', getUserName, false);
+subButton.addEventListener('click', getInput, false);
