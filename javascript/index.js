8702d9d12c1bbc3b71b7ed42978abbc5850b6d08
Zoe <zoeryan@comcast.net>
2018-10-12 18:20:59 -0700

    text imput test

--- javascript/index.js
+++ javascript/index.js
@@ -1,1 +1,11 @@
-document.getElementById('date').innerHTML = new Date().toDateString();
+// the function which handles the input field logic
+function getUserName() {
+    var nameField = document.getElementById('nameField').value;
+    var result = document.getElementById('result');
+    
+    if (nameField.length < 3) {
+        result.textContent = 'Username must contain at least 3 characters';
+
+// use an eventlistener for the event
+var subButton = document.getElementById('subButton');
+subButton.addEventListener('click', getUserName, false);
